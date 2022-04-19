1923eac7860b9775f6d65edcdc2ebfc7bfb36ae6
NicolasEQuintana <nicolasequintana@gmail.com>
2022-04-19 19:02:59 -0300

    add categories in array

--- src/components/Utils/products.js
+++ src/components/Utils/products.js
@@ -3,39 +3,44 @@ export const products = [
         id: 1,
         nombre: 'Producto 1',
         precio: '$ 1,000',
-        imagen: 'https://picsum.photos/200/300/?random'
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'remera'
     },
     {
         id: 2,
         nombre: 'Producto 2',
         precio: '$ 2,000',
-        imagen: 'https://picsum.photos/200/300/?random'
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'remera'
     },
     {
         id: 3,
         nombre: 'Producto 3',
         precio: '$ 3,000',
-        imagen: 'https://picsum.photos/200/300/?random'
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'buzo'
     },
     {
         id: 4,
         nombre: 'Producto 4',
         precio: '$ 4,000',
-        imagen: 'https://picsum.photos/200/300/?random'
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'pantalon'
     },
     {
         id: 5,
         nombre: 'Producto 5',
         precio: '$ 5,000',
-        imagen: 'https://picsum.photos/200/300/?random'
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'buzo'
+    },
+    {
+        id: 56,
+        nombre: 'Producto 6',
+        precio: '$ 6,000',
+        imagen: 'https://picsum.photos/200/300/?random',
+        categoria: 'pantalon'
     },
 ]
 
-export const product = {
-    id: 6,
-    nombre: 'Producto 6',
-    precio: '$ 6,000',
-    imagen: 'https://picsum.photos/200/300/?random'
-}
 
-console.log(products.find(p => p.id===3))
